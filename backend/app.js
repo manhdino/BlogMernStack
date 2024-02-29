f5baf39b1559432f362def6d964b4fd669fc3a1b
manhdino <manhnguyen1238@gmail.com>
2024-03-01 00:43:34 +0700

    add auth backend

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json()); //permission send JSON data
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+app.use((err, req, res, next) => {
+  //call every time request is sent to Server
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
