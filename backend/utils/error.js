111659b4b25cf909a2f4c826afd47cf373259716
manhdino <manhnguyen1238@gmail.com>
2024-03-04 00:46:18 +0700

    fix deploy part 9

--- backend/utils/error.js
+++ backend/utils/error.js
@@ -1,4 +1,4 @@
 export const errorHandler = (statusCode = 400, message = "Not Found") => {
-  const error = createError(statusCode, errorMessage);
+  const error = createError(statusCode, message);
   return error;
 };
